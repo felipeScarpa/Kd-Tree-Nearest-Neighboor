4e688376d883f66b30acf1f85f5439ae08da9d9a
felipeScarpa <felipe.tscarpa@gmail.com>
2026-07-24 03:01:13 -0300

    Update Unusual-Constraint-Knapsack.cpp

--- Materiais-Estudo/Estudo-codigos/recursao/Unusual-Constraint-Knapsack.cpp
+++ Materiais-Estudo/Estudo-codigos/recursao/Unusual-Constraint-Knapsack.cpp
@@ -2,6 +2,11 @@
 using namespace std;
 #define ll long long
 
+/*
+Primeiramente guardamos em um vetor para cada i soma dos valores de cada item de 1, 2, ... i.
+Escolhemos recursivamente e de maneira gulosas apartir do item mais pesado pegar todos os itens excluindo o mais pesado, ou pegar o mais pesado + a melhor solucao para uma instancia dessa funcao para i-1.
+*/
+
 ll n, W;
 
 vector<ll>v(61);
